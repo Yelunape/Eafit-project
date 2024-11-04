0f22b30a97155a892952fb38296a2d4ad87def95
unknown <dhincapig1@eafit.edu.co>
2024-11-04 00:55:52 -0500

    feature/feature/estado_oportunidad/creacion api estado oportunidad

--- src/main/java/com/nodoantivirus/backend/estado_oportunidad/model/EstadoOportunidad.java
+++ src/main/java/com/nodoantivirus/backend/estado_oportunidad/model/EstadoOportunidad.java
@@ -1,4 +1,37 @@
 package com.nodoantivirus.backend.estado_oportunidad.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "estado_oportunidad")
 public class EstadoOportunidad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_oportunidad", nullable = false)
+    private Oportunidad oportunidad;
+
+    @Column(name = "id_oportunidad", nullable = false)
+    private Long idOportunidad;
+
+    //Getters y setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdOportunidad() {
+        return idOportunidad;
+    }
+
+    public void setIdOportunidad(Long idOportunidad) {
+        this.idOportunidad = idOportunidad;
+    }
 }
--- src/main/java/com/nodoantivirus/backend/estado_oportunidad/repository/EstadoOportunidadRepository.java
+++ src/main/java/com/nodoantivirus/backend/estado_oportunidad/repository/EstadoOportunidadRepository.java
@@ -1,4 +1,10 @@
 package com.nodoantivirus.backend.estado_oportunidad.repository;
 
-public interface EstadoOportunidadRepository {
+
+import com.nodoantivirus.backend.estado_oportunidad.model.EstadoOportunidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoOportunidadRepository extends JpaRepository<EstadoOportunidad, Long> {
 }
