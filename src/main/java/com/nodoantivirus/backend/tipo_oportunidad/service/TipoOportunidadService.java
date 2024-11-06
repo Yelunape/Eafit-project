61e737ebcd9629d6d1a1b6b7477d390e8f6a663e
Yesenia Luna <yelunape@gmail.com>
2024-11-06 09:44:49 -0500

    feature/feature/tipos_oportunidades/tarea tipo_oportunidades completa.

--- src/main/java/com/nodoantivirus/backend/tipo_oportunidad/service/TipoOportunidadService.java
+++ src/main/java/com/nodoantivirus/backend/tipo_oportunidad/service/TipoOportunidadService.java
@@ -1,4 +1,44 @@
 package com.nodoantivirus.backend.tipo_oportunidad.service;
 
+import com.nodoantivirus.backend.tipo_oportunidad.model.TipoOportunidad;
+import com.nodoantivirus.backend.tipo_oportunidad.repository.TipoOportunidadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TipoOportunidadService {
+
+    @Autowired
+    private TipoOportunidadRepository tipoOportunidadRepository;
+
+    //Crear
+    public TipoOportunidad crearTipoOpor(TipoOportunidad tipoOportunidad) {
+        return tipoOportunidadRepository.save(tipoOportunidad);
+    }
+
+    //obtener todo
+    public List<TipoOportunidad> obtenerTiposOpor() {
+        return tipoOportunidadRepository.findAll();
+    }
+
+    //Obtener por id
+    public Optional<TipoOportunidad> obtenerTipoOporById(Long id) {
+        return tipoOportunidadRepository.findById(id);
+    }
+
+    //Actualizar
+    public TipoOportunidad actualizarTipoOpor(Long id, TipoOportunidad tipoOporDetails) {
+        TipoOportunidad tipoOportunidad = tipoOportunidadRepository.findById(id).orElseThrow();
+        tipoOportunidad.setNombre(tipoOporDetails.getNombre());
+        tipoOportunidad.setDescripcion(tipoOporDetails.getDescripcion());
+        return tipoOportunidadRepository.save(tipoOportunidad);
+    }
+
+    //Eliminar
+    public void eliminarTipoOpor(Long id) {
+        tipoOportunidadRepository.deleteById(id);
+    }
 }
