72cf21ece64211a2fed70046a25c781998df0d51
unknown <dhincapig1@eafit.edu.co>
2024-11-06 23:51:57 -0500

    creación api roles

--- src/main/java/com/nodoantivirus/backend/roles/model/Roles.java
+++ src/main/java/com/nodoantivirus/backend/roles/model/Roles.java
@@ -1,4 +1,37 @@
 package com.nodoantivirus.backend.roles.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Roles")
 public class Roles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    // Constructores, getters y setters
+    public Roles() {}
+
+    public Roles(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
