b10ff08f24e8e602494b483840be8d944b6cbe8d
Auxiliar Contable <auxiliar.contable@CITYRAIZ_SAS.LOCAL>
2024-11-07 17:32:09 -0500

    feature/feature/usuarios/Avance Usuario Servicio

--- src/main/java/com/nodoantivirus/backend/usuarios/model/Usuarios.java
+++ src/main/java/com/nodoantivirus/backend/usuarios/model/Usuarios.java
@@ -1,4 +1,16 @@
 package com.nodoantivirus.backend.usuarios.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "usuario")
+@Data
 public class Usuarios {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// Sea autoincrementable
+    private Long id;
+
+    private String nombre;
 }
+
