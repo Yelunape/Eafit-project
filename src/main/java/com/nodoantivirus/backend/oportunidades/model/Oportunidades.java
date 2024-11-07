263cd1f18c99ccc981e67a05e7e126caad4c3ec0
DianaKrol <karolopez1010@gmail.com>
2024-11-06 22:31:28 -0500

    feature/oportunidades

--- src/main/java/com/nodoantivirus/backend/oportunidades/model/Oportunidades.java
+++ src/main/java/com/nodoantivirus/backend/oportunidades/model/Oportunidades.java
@@ -1,4 +1,18 @@
 package com.nodoantivirus.backend.oportunidades.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tipo_oportunidad")
+@Getter @Setter
 public class Oportunidades {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String descripcion;
+
 }
