3ce5eac431c277a72c37aac464a70fed6c7ec970
DianaKrol <karolopez1010@gmail.com>
2024-11-07 20:04:43 -0500

    Se agrega Usuario Oportunidades

--- src/main/java/com/nodoantivirus/backend/usuario_oportunidades/controller/UsuarioOportunidadesController.java
+++ src/main/java/com/nodoantivirus/backend/usuario_oportunidades/controller/UsuarioOportunidadesController.java
@@ -1,4 +1,56 @@
 package com.nodoantivirus.backend.usuario_oportunidades.controller;
 
+import com.nodoantivirus.backend.usuario_oportunidades.model.UsuarioOportunidades;
+import com.nodoantivirus.backend.usuario_oportunidades.service.UsuarioOportunidadesService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/usuarioOportunidades")
 public class UsuarioOportunidadesController {
+
+    @Autowired
+    private UsuarioOportunidadesService usuarioOportunidadesService;
+
+    //Crear
+    @Operation(summary = "Método post para crear usuario oportunidades.", description = "Parámetros requeridos: nombre y descripción, el id se genera automáticamente asi que no es necesario agregarlo.")
+    @PostMapping
+    public UsuarioOportunidades crearUO(@RequestBody UsuarioOportunidades tipoOportunidad) {
+        return usuarioOportunidadesService.crearUO(tipoOportunidad);
+    }
+
+    //Obtener todo
+    @Operation(summary = "Método para obtener todos usuarios oportunidades", description = "No requiere parámetro.")
+    @GetMapping
+    public List<UsuarioOportunidades> obtenerTodo() {
+        return usuarioOportunidadesService.obtenerUO();
+    }
+
+    //Obtener por Id
+    @Operation(summary = "Método para obtener usuarios oportunidades por id.", description = "El único parámetro requerido es el del Id.")
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioOportunidades> obtenerPorId(@PathVariable Long id) {
+        Optional<UsuarioOportunidades> usuarioOportunidades = usuarioOportunidadesService.obtetenerUasuarioOportunidadesById(id);
+        return usuarioOportunidades.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    //Actualizar
+    @Operation(summary = "Método para actualizar un tipo de oportunidad", description = "Parámetros que se pueden editar: nombre y descripción, el id se genera automáticamente asi que no es posible modificarlo.")
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioOportunidades> actualizarUO(@PathVariable Long id, @RequestBody UsuarioOportunidades uODetails) {
+        return ResponseEntity.ok(UsuarioOportunidadesService.actualizarUO(id, uODetails));
+    }
+
+    //Eliminar
+    @Operation(summary = "Método para eliminar tipos de oportunidad por id.", description = "El único parámetro requerido es el del Id.")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> elimiUO(@PathVariable Long id) {
+        usuarioOportunidadesService.eliminarUO(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/nodoantivirus/backend/usuario_oportunidades/service/UsuarioOportunidadesService.java
+++ src/main/java/com/nodoantivirus/backend/usuario_oportunidades/service/UsuarioOportunidadesService.java
@@ -1,4 +1,55 @@
 package com.nodoantivirus.backend.usuario_oportunidades.service;
 
+import com.jayway.jsonpath.internal.path.ArraySliceOperation;
+import com.nodoantivirus.backend.usuario_oportunidades.model.UsuarioOportunidades;
+import com.nodoantivirus.backend.usuario_oportunidades.repository.UsuarioOportunidadesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UsuarioOportunidadesService {
+
+    @Autowired
+    private UsuarioOportunidadesRepository usuarioOportunidadesRepository;
+
+    //Crear
+        public UsuarioOportunidades crearUO(@RequestBody UsuarioOportunidades usuarioOportunidades) {
+        return usuarioOportunidadesRepository.save(usuarioOportunidades);
+    }
+
+    //Obtener todo
+    public List<UsuarioOportunidades> obtenerUO() {
+        return usuarioOportunidadesRepository.findAll();
+    }
+
+    //Obtener por Id
+    public Optional<UsuarioOportunidades> obtenerUsuarioOportunidadesporByIdId(Long id) {
+        return usuarioOportunidadesRepository.findById(id);
+    }
+
+    //Actualizar
+    public UsuarioOportunidades actualizarUO(Long id, UsuarioOportunidades usuarioOportunidadesDetails) {
+        UsuarioOportunidades usuarioOportunidades = usuarioOportunidadesRepository.findById(id).orElseThrow();
+        usuarioOportunidades.setNombre(usuarioOportunidadesDetails.getNombre());
+        usuarioOportunidades.setDescripcion(usuarioOportunidadesDetails.getDescripcion());
+        return usuarioOportunidadesRepository.save(usuarioOportunidades);
+    }
+
+    //Eliminar
+    @ArraySliceOperation.Operation(summary = "Método para eliminar usuario oportunidades por id.", description = "El único parámetro requerido es el del Id.")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarUO(@PathVariable Long id) {
+        usuarioOportunidadesService.eliminarUO(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
